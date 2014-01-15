f312c6be8740156639aed99f0cdfda0aa23c8717
akameco <akame.makers@gmail.com>
2014-01-16 03:21:39 +0900

    Kasi-time:add select button

--- Kasi-time.user.js
+++ Kasi-time.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://twitter.com/akameco
 // @description 不可能を可能にする
 // @include     http://www.kasi-time.com/*
-// @version     1
+// @version     1.00
 // @grant       none
 // ==/UserScript==
 
@@ -17,17 +17,20 @@ function enableCopyText() {
 
 // select all textarea
 function selectText() {
-  var element= document.querySelector(".mainkashi");
-  //var element= document.querySelector(document.body);
+  let element= document.querySelector(".mainkashi");
   // create range
-  var rng = document.createRange();
+  let rng = document.createRange();
   rng.selectNodeContents(element);
   // add range for the selected regions
   window.getSelection().addRange(rng);
 }
 
 function createCopyButton() {
-  selectText();
+  let button = document.createElement("button");
+  button.innerHTML = "全て選択";
+  button.addEventListener("click",function(){selectText()},false);
+  let frame = document.querySelector("#kashi_flame");
+  frame.appendChild(button);
 }
 
 window.onload = function () {
