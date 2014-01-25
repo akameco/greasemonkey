078f2c49d500e0333fbd1158343cb19bcf201a7b
akameco <akame.makers@gmail.com>
2014-01-26 01:45:29 +0900

    ATB:hostによって実行するメソッドを変更

--- amazon-TDU-bookSeach.user.js
+++ amazon-TDU-bookSeach.user.js
@@ -4,21 +4,53 @@
 // @description AmazonとTDUMediaセンターの蔵書検索をリンク
 // @author      akameco
 // @include     http://www.amazon.co.jp/*
+// @include     https://lib.mrcl.dendai.ac.jp/webopac/odrexm.do
 // @version     1.00
 // @grant       none
 // ==/UserScript==
 
 window.onload = function () {
-  // カテゴリのチェック
-  if(!checkCategory())
-    return;
-  let isbn = getIsbn();
-  // 取得確認
-  if(isbn){
-    let text = getBookData(isbn);
-    createLink(isbn);
-    loading();
-    //  addStyle();
+  var host = location.host;
+  checkHost[host]();
+}
+
+var checkHost = {
+  "www.amazon.co.jp": function () {
+    // カテゴリのチェック
+    if(!checkCategory())
+      return;
+    var isbn = getIsbn();
+    if(isbn){
+      var text = getBookData(isbn);
+      createLink(isbn);
+      loading();
+      //  addStyle();
+    }
+  },
+  "lib.mrcl.dendai.ac.jp": function () {
+    var loginButton = null;
+    var pass=false;
+    var form = document.forms[0];
+    form.setAttribute("autocomplete","on");
+    if(document.querySelectorAll('.err_cmt').length == 1) {
+      return;
+    }
+    for (let i=0; formElement=form.getElementsByTagName("input")[i]; ++i){
+      if(formElement.type == "password" && formElement.value){
+        pass = true; 
+        break;
+      }
+    }
+    for (let i=0; formElement=form.getElementsByTagName("input")[i]; ++i){
+      if (formElement.type == "image" && pass) {
+        loginButton = formElement;
+        break;
+      }
+    }
+    if(loginButton){
+      loginButton.focus();
+      loginButton.click();
+    }
   }
 }
 
@@ -32,8 +64,8 @@ function getIsbn() {
 
 // 非同期通信により蔵書情報取得
 function getBookData(isbn) {
-  let request = new XMLHttpRequest();
-  let link = 'http://lib.mrcl.dendai.ac.jp/webopac/ctlsrh.do?isbn_issn=' + isbn;
+  var request = new XMLHttpRequest();
+  var link = 'http://lib.mrcl.dendai.ac.jp/webopac/ctlsrh.do?isbn_issn=' + isbn;
   // 一度allow-any-originを噛ませることでクロスドメイン制限対策
   request.open('GET','http://allow-any-origin.appspot.com/' + link,true);
   request.send(); 
@@ -43,36 +75,9 @@ function getBookData(isbn) {
   }
 }
 
-// css定義
-// TODO:スタイルをサイトに合うように
-function addStyle() {
-  let style = "\
-  div#tdu_link{\
-    background: #4169E1;\
-    width: 180px;\
-    display: table;\
-    margin: 10px 5px;\
-    padding: 5px 2px;\
-  }\
-  div#tdu_link a{\
-    color: #D6FCFF;\
-    margin: 10px 5px;\
-    font-size: 14px;\
-  }\
-  div#tdu_link a:hover{\
-    color: #6A5ACD;\
-  }\
-  ";
-  let head = document.getElementsByTagName('head')[0];
-  let element = head.appendChild(window.document.createElement('style'));
-  element.type = "text/css";
-  element.textContent = style;
-}
-
 // カテゴリが合っているか確認
-// TODO:カテゴリチェックをurlでする関数に変更
 function checkCategory() {
-  let category = document.querySelector('.nav-category-button').firstChild.innerHTML;
+  var category = document.querySelector('.nav-category-button').firstChild.innerHTML;
   if(category == '本'){
     return true;
   }
@@ -81,15 +86,15 @@ function checkCategory() {
 
 // 図書館へのリンク
 function createLink(isbn) {
-  let div = neoCreate('div',{id:'tdu_link'});
-  let link = neoCreate('a',
+  var div = neoCreate('div',{id:'tdu_link'});
+  var link = neoCreate('a',
     {
       href:"http://lib.mrcl.dendai.ac.jp/webopac/ctlsrh.do?isbn_issn=" + isbn,
       target: '_blank'
     },
     "図書館検索"
   );
-  let p = parent.document.getElementById('btAsinTitle').parentNode;
+  var p = parent.document.getElementById('btAsinTitle').parentNode;
   div.appendChild(link);
   p.appendChild(div);
 }
@@ -109,22 +114,22 @@ function neoCreate(tag,attr,content) {
 
 // ロード状態の表示
 function loading() {
-  let div = document.createElement('div');
+  var div = document.createElement('div');
   div.setAttribute('id','loading');
   div.textContent = "now loading...";
-  let p = parent.document.getElementById('btAsinTitle').parentNode;
+  var p = parent.document.getElementById('btAsinTitle').parentNode;
   p.appendChild(div);
 }
 
 // ロード表示の削除 
 function removeLoading() {
-  let element = document.getElementById('loading');
+  var element = document.getElementById('loading');
   element.parentNode.removeChild(element);
 }
 
 // 
 function parseHtml(res) {
-  let div = document.createElement('div');
+  var div = document.createElement('div');
   div.innerHTML = res;
   getObj(div);
 }
@@ -132,30 +137,52 @@ function parseHtml(res) {
 // 状態の表示
 function getObj(html) {
   removeLoading();
-  let div = document.createElement('div');
+  var div = document.createElement('div');
   div.setAttribute('id','tduBooks');
   // 要素の調査
-  let tbody = html.querySelectorAll('.flst_head')[0].parentNode;
+  var tbody = html.querySelectorAll('.flst_head')[0].parentNode;
   for (let i=1; i < tbody.children.length; ++i) {
-    let element = document.createElement('div');
-    let tr = tbody.children[i];
-    // 所蔵館
-    let plase = tr.children[3].firstChild.firstChild.nodeValue;
-    console.log(plase);
-    // 状態
-    let state = tr.children[8].firstChild.firstChild.nodeValue;
-    console.log(state);
-    // 返却期限日
-    // 配架済 or 貸出中
-    if(state == "貸出中"){
-      let priod = tr.children[9].firstChild.firstChild.nodeValue;
-      console.log(priod);
-      element.innerHTML = plase + ":" + state + ":" + priod;
+    var element = document.createElement('div');
+    var tr = tbody.children[i];
+    // 所蔵館・状態・返却期限日(配架済 or 貸出中)
+    var library = {
+      plase: tr.children[3].firstChild.firstChild.nodeValue,
+      state: tr.children[8].firstChild.firstChild.nodeValue,
+      priod: tr.children[9].firstChild.firstChild.nodeValue
+    }
+    if(library.state == "貸出中"){
+      element.innerHTML = library.plase + " " + library.state + " " + "返却期限 " + library.priod;
     }else{
-      element.innerHTML = plase + ":" + state;
+      element.innerHTML = library.plase + " " + library.state;
     }
     div.appendChild(element);
   }
-  let p = parent.document.getElementById('btAsinTitle').parentNode;
+  var p = parent.document.getElementById('btAsinTitle').parentNode;
   p.appendChild(div);
+}
+
+// css定義
+// TODO:スタイルをサイトに合うように
+function addStyle() {
+  var style = "\
+  div#tdu_link{\
+    background: #4169E1;\
+    width: 180px;\
+    display: table;\
+    margin: 10px 5px;\
+    padding: 5px 2px;\
+  }\
+  div#tdu_link a{\
+    color: #D6FCFF;\
+    margin: 10px 5px;\
+    font-size: 14px;\
+  }\
+  div#tdu_link a:hover{\
+    color: #6A5ACD;\
+  }\
+  ";
+  var head = document.getElementsByTagName('head')[0];
+  var element = head.appendChild(window.document.createElement('style'));
+  element.type = "text/css";
+  element.textContent = style;
 } 
