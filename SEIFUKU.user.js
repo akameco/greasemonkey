9e232bdb0b236e545e066886d5ce6c817a590a1a
akameco <akame.makers@gmail.com>
2014-02-07 03:16:30 +0900

    create new script

---
+++ SEIFUKU.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name        SEIFUKU
+// @namespace   https://twitter.com/akameco
+// @description 我らがズヴィズダーの光を、あまねく世界に
+// @include     http://www.nicovideo.jp/watch/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+(function() {
+    // 要素作成
+    let createElement = function(tag,attr,content){
+      let dom = document.createElement(tag);
+      for (let key in attr) {
+        dom.setAttribute(key);
+      }
+      if(content){
+        dom.textContent = content;
+      }
+      return dom;
+    };
+
+    let Nico = {
+      // setButton 
+      setButton: function () {
+        console.log("test");
+        let button = createElement('a',{id:'seifuku_button'},'征服');
+        let p = document.getElementById('videoHeaderTagList').parent;
+        console.log(p);
+        p.appendChild(button);
+        console.log(button);
+      },
+      // ボタンが押された時のアニメーション
+      animetion: function () {
+      }
+
+    };
+    try{
+      window.onload = function() {
+        Nico.setButton();
+      };
+    }catch(err){
+      console.log(err);
+    }
+})(); 
